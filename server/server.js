3ed7f614fe7318b40e197a94f2773d9265a45ec8
Hirusha11-cpu <developer6@appletechlabs.com>
2025-03-12 14:24:00 +0530

    update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://mern-auth-drab-two.vercel.app/']
 
 app.use(express.json());
 app.use(cookieParser());
