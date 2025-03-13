eb91358b61ac7733476ef010e5f32d62a59621ba
Hirusha1-cpu <hirushafernando121@gmail.com>
2025-03-13 11:44:01 +0530

    update

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
-import connectDB from './config/mongodb.js'
-import authRouter from './routes/authRoutes.js';
-import userRouter from './routes/userRoutes.js';
+import connectDB from '../config/mongodb.js'
+import authRouter from '../routes/authRoutes.js';
+import userRouter from '../routes/userRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
